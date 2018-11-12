5603388f8c3031cbda5a26ac625b9873cd51ca8f
jtylerray <jtray@outlook.es>
2018-11-11 17:19:58 -0700

    getting ready to host

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.json())
 const {PORT} = process.env
+app.use(express.static(`${s__dirname}/build`))
 
 app.get('/api/checkfunction', (req, res)=>{res.send('that worked!').status(200)})
 
