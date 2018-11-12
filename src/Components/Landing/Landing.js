8c6d6a5db507b6e79a5b78de33216780416950af
jtylerray <jtray@outlook.es>
2018-11-11 20:20:16 -0700

    working ed

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -6,6 +6,7 @@ function Start() {
     return (
         <div className="Landing-wrapper">
             <div className="empty">
+            <h1 style = {{"color":"red", "font-size":"36pt"}}>Please keep in mind this website is still under production by J. Tyler Ray as a clone. If you're looking for Brit's website please <a href="https://britlamour.com/home">Click Here!</a></h1>
             </div>
             <div className="Enter-Site">
                 <div className="enter-wrapper">
