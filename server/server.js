f9c140a08743cc198319e01b8084555787858f25
jtylerray <jtray@outlook.es>
2018-11-11 18:07:22 -0700

    testing

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.json())
 // const {PORT} = process.env
-const port = process.env.PORT
-app.use(express.static(`${__dirname}/build`))
+const port = 5000
+console.log('port =',process.env.PORT)
 
 app.get('/api/checkfunction', (req, res)=>{res.send('that worked!').status(200)})
 
 
+app.use(express.static(`${__dirname}/build`))
 app.listen(port, () =>{ console.log(`Server listening on port ${port}`)})
 
