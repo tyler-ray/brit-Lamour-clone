4aceb1fe80136d8c94aaf6724eda9de84c8eebd6
jtylerray <jtray@outlook.es>
2018-11-11 20:14:25 -0700

    working on lots

--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
 const express = require ('express')
-require('dotenv').config()
 const bodyParser = require('body-parser')
 const app = express();
+const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
+app.use(express.static(`${__dirname}/../build`))
 app.use(bodyParser.json())
+
+app.use(cors())
+app.options('*',cors())
+
+
 // const {SERVER_PORT} = process.env
+let {
+    SERVER_PORT,
+} = process.env
+
+
+
+
 const server_port = 5000
-console.log('port =',process.env.SERVER_PORT)
+console.log('port =', SERVER_PORT)
 app.get('/api/checkfunction', (req, res)=>{res.send('that worked!').status(200)})
+app.get('/api/store',(req,res)=>{
+    res.send([abc,def,ghi,jkl])
+})
 
 
 
-app.use(express.static(`${__dirname}/build`))
 app.listen(server_port, () =>{ console.log(`Server listening on port ${server_port}`)})
 
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 function Shop (){
+
+    function request (){
+        axios.get('http://britlamour.jtylerray.com/api/checkfunction').then((res)=>{console.log('response',res.data)})
+    }
     return(
         <div className="Shop">
             Shop
+            <button onClick={request}> Click me!</button>
         </div>
     )
 } 
