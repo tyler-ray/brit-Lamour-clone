67a433f91a08565f5e3e7a1f01f22b586080b90b
jtylerray <jtray@outlook.es>
2018-11-11 18:00:12 -0700

    testing

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ require('dotenv').config()
 const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.json())
-const {PORT} = process.env
+// const {PORT} = process.env
+const port = process.env.PORT
 app.use(express.static(`${__dirname}/build`))
 
 app.get('/api/checkfunction', (req, res)=>{res.send('that worked!').status(200)})
 
 
-app.listen(PORT, () =>{ console.log(`Server listening on port ${PORT}`)})
+app.listen(port, () =>{ console.log(`Server listening on port ${port}`)})
 
