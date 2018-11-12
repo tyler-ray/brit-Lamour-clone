ac4b77c2d42e4a42f68396552a980e8b97689ff6
jtylerray <jtray@outlook.es>
2018-11-11 18:51:39 -0700

    changing ports

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.json())
-// const {PORT} = process.env
-const port = 5000
-console.log('port =',process.env.PORT)
-
+// const {SERVER_PORT} = process.env
+const server_port = 5000
+console.log('port =',process.env.SERVER_PORT)
 app.get('/api/checkfunction', (req, res)=>{res.send('that worked!').status(200)})
 
 
+
 app.use(express.static(`${__dirname}/build`))
-app.listen(port, () =>{ console.log(`Server listening on port ${port}`)})
+app.listen(server_port, () =>{ console.log(`Server listening on port ${server_port}`)})
 
